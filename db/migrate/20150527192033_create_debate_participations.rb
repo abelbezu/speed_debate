752183975a1ec53d1426f2f63b3e4a5eb43524e1
Abel <abelbezu@mit.edu>
2015-05-29 21:27:26 -0400

    deploy fixed

--- db/migrate/20150527192033_create_debate_participations.rb
+++ db/migrate/20150527192033_create_debate_participations.rb
@@ -1,5 +1,5 @@
 class CreateDebateParticipations < ActiveRecord::Migration
-  def up
+  def change
     create_table :debate_participations do |t|
       t.references :account
       t.references :debate
@@ -10,7 +10,5 @@ class CreateDebateParticipations < ActiveRecord::Migration
     end
     add_index :debate_participations, ["account_id", "debate_id"]
   end
-  def down
-  	drop_table :debate_participations
-  end 
+  
 end
